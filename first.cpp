5d13fa79ca71f7a65e4fe53e50f907c8be30646c
antonisman <160828119+Antechn@users.noreply.github.com>
2024-02-22 11:16:40 +0200

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int sum=0;
+int a;
+int b;
+cout <<"dose mou duo arithmous:"<< endl;
+  scanf("%d",&a);
+  scanf("%d",&b);
+sum=a+b;
+cout<<"to arthrisma einai "<< sum;
+return 0;
+}
