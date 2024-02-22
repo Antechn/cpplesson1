c9eb49f8af972cf3413b8616904f41aa57188814
antonisman <160828119+Antechn@users.noreply.github.com>
2024-02-22 11:44:28 +0200

    Update question.cpp

--- question.cpp
+++ question.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream> 
+using namespace std; 
+int main() 
+{
+	char reply; /* dinxei mono ena gramma */
+cout << "dose mou nai h oxi: ";
+scanf("%c", &reply);
+cout << "apantise" << endl << reply;
+return 0;
+}
